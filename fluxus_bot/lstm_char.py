e1a400ee6b1df3d78f6e7fd214c609ff800faadb
Anton Alekseev <anton.m.alexeyev@gmail.com>
2017-06-23 13:26:25 +0300

    LSTM added to bot

--- fluxus_bot/lstm_char.py
+++ fluxus_bot/lstm_char.py
@@ -86,31 +86,30 @@ for iteration in range(3):
 
     # start_index = random.randint(0, len(text) - maxlen - 1)
 
-    for diversity in [0.1, 0.2, 0.5, 1.0, 1.2, 2.0]:
+    # for diversity in [0.1, 0.2, 0.5, 1.0, 1.2, 2.0]:
+    #
+    #     print()
+    #     print('----- diversity:', diversity)
 
-        print()
-        print('----- diversity:', diversity)
-
-        generated = ''
-        # sentence = text[start_index: start_index + maxlen]
-        sentence = " " * 31 + "[s]  <n> "
-        generated += sentence
+        # generated = ''
+        # # sentence = text[start_index: start_index + maxlen]
+        # sentence = " " * 31 + "[s]  <n> "
+        # generated += sentence
         # print('----- Generating with seed: "' + sentence + '"')
         # sys.stdout.write(generated)
-
-        for i in range(400):
-
-            x = np.zeros((1, len(sentence), len(chars)))
-
-            for t, char in enumerate(sentence):
-                x[0, t, char_indices[char]] = 1.
-
-            preds = model.predict(x, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_char = indices_char[next_index]
-
-            generated += next_char
-            sentence = sentence[1:] + next_char
+        # for i in range(400):
+        #
+        #     x = np.zeros((1, len(sentence), len(chars)))
+        #
+        #     for t, char in enumerate(sentence):
+        #         x[0, t, char_indices[char]] = 1.
+        #
+        #     preds = model.predict(x, verbose=0)[0]
+        #     next_index = sample(preds, diversity)
+        #     next_char = indices_char[next_index]
+        #
+        #     generated += next_char
+        #     sentence = sentence[1:] + next_char
 
             # sys.stdout.write(next_char)
             # sys.stdout.flush()
